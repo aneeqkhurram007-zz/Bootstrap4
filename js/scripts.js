b6792bf41bdaebc294747036af243844a8d526a0
aneeqkhurram007 <aneeqkhurram007@yahoo.com>
2021-01-08 20:47:30 +0500

    8.47

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    $('[data-toggle="tooltip"').tooltip();
+
+    $("#mycarousel").carousel({ interval: 2000 });
+    $("#carouselButton").click(function () {
+        if ($("#carouselButton").children("span").hasClass("fa-pause")) {
+            $("#mycarousel").carousel("pause");
+            $("#carouselButton").children("span").removeClass("fa-pause");
+            $("#carouselButton").children("span").addClass("fa-play");
+        } else {
+            $("#mycarousel").carousel("cycle");
+            $("#carouselButton").children("span").removeClass("fa-play");
+            $("#carouselButton").children("span").addClass("fa-pause");
+        }
+    });
+    $("#loginLinkButton").hover(function () {
+        $("#loginModal").modal("show");
+    });
+    $("#reserveTableButton").click(function () {
+        $("#reserveModal").modal();
+    });
+});
